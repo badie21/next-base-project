339872edb3620bc2f892f0e2d4c1b99139b32d14
محمد بادیه نشین <smohbad@sinacomsys.local>
2022-07-03 16:41:24 +0430

    testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => (
+  //
   <div className={styles.container}>
     <Head>
       <title>Create Next App</title>
